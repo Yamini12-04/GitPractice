5ceaef6c4a4df7895559a0743ff4069ccdeb8256
Yamini <saiyamini815@gmail.com>
2025-09-13 22:53:40 +0530

    Changes made

---
+++ src/main/java/org/example/XExample.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class XExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			System.out.println("Driving the car");	
+			System.out.println("Changes made");
+	}
+
+}
